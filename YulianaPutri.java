21e554319c27fa3dd1393bd200c101d65620b621
yulianaputri <43033218+yulianaputri@users.noreply.github.com>
2018-09-07 10:57:48 +0700

    Add files via upload

---
+++ YulianaPutri.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yulianaputri;
+
+/**
+ *
+ * @author Yuliana Putri
+ */
+public class YulianaPutri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for (int j=1;j<=jumlah;j++){
+            for(int y=1;y<=j;y++){
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+    
+}
